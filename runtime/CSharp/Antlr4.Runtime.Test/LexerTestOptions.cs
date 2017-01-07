efd70dc1f48db07cd3095ddc78d94ab644294452
Sam Harwell <sam@tunnelvisionlabs.com>
2017-01-06 23:36:45 -0600

    Run the test suite in C# instead of Java
    This commit updates the test suite to only generate C# code. The compilation
    and execution of the test code is left for the various Antlr4.Runtime.Test
    projects. The strategy results in a 20x improvement for generating and
    executing the test suite, and over a 100x improvement for rerunning tests
    without generating them again.
    
    This work is heavily based on the approach used by @BurtHarris in
    tunnelvisionlabs/antlr4ts#244.

---
+++ runtime/CSharp/Antlr4.Runtime.Test/LexerTestOptions.cs
@@ -0,0 +1,45 @@
+﻿namespace Antlr4.Runtime.Test
+{
+    using System;
+
+    internal class LexerTestOptions
+    {
+        internal delegate TResult Factory<TArg, TResult>(TArg argument);
+
+        public string TestName
+        {
+            get;
+            set;
+        }
+
+        public Factory<ICharStream, Lexer> Lexer
+        {
+            get;
+            set;
+        }
+
+        public string Input
+        {
+            get;
+            set;
+        }
+
+        public string ExpectedOutput
+        {
+            get;
+            set;
+        }
+
+        public string ExpectedErrors
+        {
+            get;
+            set;
+        }
+
+        public bool ShowDFA
+        {
+            get;
+            set;
+        }
+    }
+}
