1ad6752cb5ddfb8f262a0c1c44a152688b0c1274
Sam Harwell <sam@tunnelvisionlabs.com>
2017-01-13 06:51:44 -0600

    Update all copyright headers for consistency following merge

--- runtime/CSharp/Antlr4/Program.cs
+++ runtime/CSharp/Antlr4/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace Antlr4
+﻿// Copyright (c) Terence Parr, Sam Harwell. All Rights Reserved.
+// Licensed under the BSD License. See LICENSE.txt in the project root for license information.
+
+namespace Antlr4
 {
     using Environment = System.Environment;
 
--- runtime/CSharp/Antlr4/Properties/AssemblyInfo.cs
+++ runtime/CSharp/Antlr4/Properties/AssemblyInfo.cs
@@ -1,4 +1,7 @@
-﻿using System.Reflection;
+﻿// Copyright (c) Terence Parr, Sam Harwell. All Rights Reserved.
+// Licensed under the BSD License. See LICENSE.txt in the project root for license information.
+
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
