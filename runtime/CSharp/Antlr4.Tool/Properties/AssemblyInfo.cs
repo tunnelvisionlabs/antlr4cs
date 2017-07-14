a381f145e297f90f297c9d8997f69db1d5b3f8bd
Sam Harwell <sam@tunnelvisionlabs.com>
2017-07-14 16:04:53 -0500

    Convert Antlr4.Tool to the new project system

--- runtime/CSharp/Antlr4.Tool/Properties/AssemblyInfo.cs
+++ runtime/CSharp/Antlr4.Tool/Properties/AssemblyInfo.cs
@@ -5,12 +5,6 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Antlr4.Tool")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Tunnel Vision Laboratories, LLC")]
-[assembly: AssemblyProduct("Antlr4")]
-[assembly: AssemblyCopyright("Copyright © Sam Harwell 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: CLSCompliant(false)]
@@ -22,17 +16,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("80df5b27-a8b9-4b6d-8cf2-314e8bd772f8")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("4.6.0.0")]
-[assembly: AssemblyFileVersion("4.6.1.0")]
-[assembly: AssemblyInformationalVersion("4.6.1-dev")]
