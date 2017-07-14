dc33fe4af6f594929270bbef09de383f72cecc9e
Sam Harwell <sam@tunnelvisionlabs.com>
2017-07-14 16:46:23 -0500

    Migrate Antlr4BuildTasks to the new project system

--- runtime/CSharp/Antlr4BuildTasks/Properties/AssemblyInfo.cs
+++ runtime/CSharp/Antlr4BuildTasks/Properties/AssemblyInfo.cs
@@ -8,12 +8,6 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Antlr4BuildTasks")]
-[assembly: AssemblyProduct("Antlr4BuildTasks")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Tunnel Vision Laboratories, LLC")]
-[assembly: AssemblyCopyright("Copyright © Sam Harwell 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -25,17 +19,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("e79060d9-d211-4367-925a-5541943d3857")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("4.6.0.0")]
-[assembly: AssemblyFileVersion("4.6.5.0")]
-[assembly: AssemblyInformationalVersion("4.6.5-dev")]
