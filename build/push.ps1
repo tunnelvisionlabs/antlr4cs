93b135a8eea862967757e01b66241360bb8d3726
Sam Harwell <sam@tunnelvisionlabs.com>
2018-05-13 17:02:40 -0500

    Update push.ps1 to account for the removal of Antlr4.VS2008

--- build/push.ps1
+++ build/push.ps1
@@ -12,18 +12,17 @@ If ($AntlrVersion.EndsWith('-dev')) {
 $packages = @(
 	'Antlr4.Runtime'
 	'Antlr4.CodeGenerator'
-	'Antlr4'
-	'Antlr4.VS2008')
+	'Antlr4')
 
 # Make sure all packages exist before pushing any packages
 ForEach ($package in $packages) {
 	If (-not (Test-Path ".\nuget\$package.$AntlrVersion.nupkg")) {
-		$host.ui.WriteErrorLine("Couldn't locate NuGet package: $JarPath")
+		$host.ui.WriteErrorLine("Couldn't locate NuGet package: $package.$AntlrVersion.nupkg")
 		exit 1
 	}
 
 	If (-not (Test-Path ".\nuget\$package.$AntlrVersion.symbols.nupkg")) {
-		$host.ui.WriteErrorLine("Couldn't locate NuGet symbols package: $JarPath")
+		$host.ui.WriteErrorLine("Couldn't locate NuGet symbols package: $package.$AntlrVersion.symbols.nupkg")
 		exit 1
 	}
 }
